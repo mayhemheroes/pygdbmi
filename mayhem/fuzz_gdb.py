e2ee049914a770f0dbc6ef65df6d75b3532ec9a6
Bailey Capuano <bcapuano@asu.edu>
2022-12-28 23:23:52 -0500

    Initial MayhemHeroes Integration

---
+++ mayhem/fuzz_gdb.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import atheris
+import sys
+import fuzz_helpers
+
+with atheris.instrument_imports():
+    from pygdbmi import gdbmiparser
+
+def TestOneInput(data):
+    fdp = fuzz_helpers.EnhancedFuzzedDataProvider(data)
+    try:
+        gdbmiparser.parse_response(fdp.ConsumeRandomString())
+    except (TypeError, ValueError):
+        return -1
+def main():
+    atheris.Setup(sys.argv, TestOneInput)
+    atheris.Fuzz()
+
+
+if __name__ == "__main__":
+    main()
